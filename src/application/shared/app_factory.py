c6719394b068859e299f34ebf3a3a4ca14fc06fc
berestianyi <berestvania@gmail.com>
2025-09-09 01:54:21 +0300

    add new router to app

--- src/application/shared/app_factory.py
+++ src/application/shared/app_factory.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from starlette.staticfiles import StaticFiles
+
 from src.application.config import settings
 from src.interfaces.api.routers.tasks import task_router
+from src.interfaces.api.routers.users import user_api_router
 
 
 def create_app() -> FastAPI:
@@ -20,5 +23,7 @@ def create_app() -> FastAPI:
     )
 
     app.include_router(task_router)
+    app.include_router(user_api_router)
 
+    app.mount("/files/csv", StaticFiles(directory=settings.USERS_CSV_PATH, check_dir=True), name="csv")
     return app
