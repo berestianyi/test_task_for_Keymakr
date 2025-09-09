bc058da4d3f2cd3e8dd64b7f7afc270c6d9cf12c
berestianyi <berestvania@gmail.com>
2025-09-09 16:57:53 +0300

    add ml_router for app

--- src/application/shared/app_factory.py
+++ src/application/shared/app_factory.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 
 from src.application.config import settings
+from src.interfaces.api.routers.ml import ml_router
 from src.interfaces.api.routers.tasks import task_router
 from src.interfaces.api.routers.users import user_api_router
 
@@ -24,6 +25,7 @@ def create_app() -> FastAPI:
 
     app.include_router(task_router)
     app.include_router(user_api_router)
+    app.include_router(ml_router)
 
     app.mount("/files/csv", StaticFiles(directory=settings.USERS_CSV_PATH, check_dir=True), name="csv")
     return app
